9103420012d17c71804d3e71c3c25e62b3634d23
Michael Elliott <elliotec@Michaels-MacBook-Air.local>
2014-01-17 12:56:39 -0800

    rearrange and reprecompile assets

--- test/features/domain_ping_test.rb
+++ test/features/domain_ping_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 feature "can ping domain" do
   scenario "find localhost" do
-    visit "localhost:3000" 
+    visit("http://localhost:3000") 
     assert page.has_content?("Michael")
   end
 end
