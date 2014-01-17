c64d4482d7dcfd322439c30884bd13edfa0b2654
Michael Elliott <elliotec@Michaels-MacBook-Air.local>
2014-01-17 13:34:03 -0800

    fixing images

--- test/features/domain_ping_test.rb
+++ test/features/domain_ping_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 feature "can ping domain" do
   scenario "find localhost" do
-    visit("http://localhost:3000") 
+    visit("http://elliotec.herokuapp.com") 
     assert page.has_content?("Michael")
   end
 end
