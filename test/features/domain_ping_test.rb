12c6bddf74e0a1138bb311c596e1bddd847f7321
elliotec <elliotecweb@gmail.com>
2014-01-19 22:23:56 -0800

    change localhost ping to domain ping

--- test/features/domain_ping_test.rb
+++ test/features/domain_ping_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 feature "can ping domain" do
-  scenario "find localhost" do
+  scenario "find domain name" do
     visit("http://elliotec.herokuapp.com") 
     assert page.has_content?("Michael")
   end
