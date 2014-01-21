ea93c2d45252e77c004b89d5f8076a1e25d41d28
elliotec <elliotecweb@gmail.com>
2014-01-20 21:18:38 -0800

    MVC from scratch

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ActiveRecord::Base
+  validates :technologies_used, presence: true
+  validates :name, length: { in: 3..255 }
 end
---
+++ test/features/projects/editing_a_project_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+feature "As a site owner, I want to be able to edit a project so that I can correct typos" do
+  scenario "editing an existing project" do
+    # Given an existing post
+    visit edit_project_path(projects(:portfolio))
+    # When I make changes
+    fill_in "Name", with: "Sweet Portfolio!"
+    click_on "Update Project"
+    # Then the changes should be saved and shown
+    page.text.must_include "Sweet Portfolio!"
+    page.text.wont_include "Legitimizable"
+  end
+
+  scenario "incorrectly editing an existing project" do
+    visit edit_project_path(projects(:portfolio))
+      fill_in "Name", with: "Er"
+      click_on "Update Project"
+      page.text.must_include "prohibited"
+      page.text.must_include "Name is too short"
+  end
+end
