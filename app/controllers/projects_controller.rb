896bba0d5f87f4d0d22a89cab732113318423019
elliotec <elliotecweb@gmail.com>
2014-01-21 09:22:55 -0800

    fix delete funtionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
     @projects = Project.all
@@ -44,6 +45,10 @@ class ProjectsController < ApplicationController
   end
 
 private
+
+    def set_project
+      @project = Project.find(params[:id])
+    end
 
     def project_params
       params.require(:project).permit(:name,:technologies_used)
