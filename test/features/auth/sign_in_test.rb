42bca9429a51ca0b02ead8f1ee740df4dfeb6b5e
elliotec <elliotecweb@gmail.com>
2014-01-23 10:57:10 -0800

    test correction

--- test/features/auth/sign_in_test.rb
+++ test/features/auth/sign_in_test.rb
@@ -4,9 +4,10 @@ feature "sign in as an existing user" do
   scenario "click the sign in link" do
     visit edit_user_registration_path
     fill_in "Email", with: users(:us).email
-    fill_in "Password", with: users(:us).encrypted_password
+    fill_in "Password", with: "password1"
     click_on "Sign in"
     #page.find("[type='submit']").click
+    save_and_open_page
     page.text.must_include "successfully"
   end
 end
--- test/features/auth/sign_up_test.rb
+++ test/features/auth/sign_up_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 feature "try to sign up as a user" do
   scenario "click the sign up link" do
     visit new_user_registration_path
-    fill_in "Email", with: "judas@thebible.com"
+    fill_in "Email", with: 'bruce@wayne.com'
     fill_in "Password", with: "password1"
     fill_in "Password confirmation", with: "password1"
     page.find("[type='submit']").click
