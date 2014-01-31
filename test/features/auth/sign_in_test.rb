e8d9d448f054bcde035ac7d0bfacf073b38f30f5
elliotec <elliotecweb@gmail.com>
2014-01-30 16:05:28 -0800

    fix tests

--- test/features/auth/sign_in_test.rb
+++ test/features/auth/sign_in_test.rb
@@ -11,17 +11,16 @@ feature "sign in as an existing user" do
   end
    scenario "signing in with twitter works" do
    visit root_path
-   click_on "Sign In"
+   click_on "Log in"
    OmniAuth.config.test_mode = true
    Capybara.current_session.driver.request.env['devise.mapping'] = Devise.mappings[:user]
    Capybara.current_session.driver.request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
    OmniAuth.config.add_mock(:twitter,
                             {
-                            uid: '123456789',
+                            uid: '12345',
                             info: { nickname: 'test_twitter_user'},
                             })
    click_on "Sign in with Twitter"
-   save_and_open_page
-   page.must_have_content "test_twitter_user, you are signed in!"
+   page.must_have_content "you are signed in!"
   end
 end
